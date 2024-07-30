41c97c796ddda6b4d7d99ee4005189849b529b61
phuongh2tnt <166468012+phuongh2tnt@users.noreply.github.com>
2024-07-31 01:12:47 +0700

    Update trainunetpre4090.py

--- train/trainunetpre4090.py
+++ train/trainunetpre4090.py
@@ -7,6 +7,11 @@ import numpy as np
 import argparse
 from torch.optim import Adam
 import utils.metrics as metrics
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from utils.metric2 import accuracy, iou, f1, precision, recall 
+from torch.amp import GradScaler, autocast
 
 
 # Setup CUDA
@@ -23,7 +28,7 @@ def setup_cuda():
     return torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-def train_model():
+def train_model(accumulation_steps=2):
     """
     Train the model over a single epoch
     :return: training loss and segmentation performance
@@ -31,7 +36,8 @@ def train_model():
     model.train()
     train_loss = 0.0
     performance = 0
-
+    train_metrics = {'iou': 0, 'accuracy': 0, 'precision': 0, 'recall': 0, 'f1': 0}
+    scaler = GradScaler('cuda')
     for i, (img, gt) in enumerate(tqdm(train_loader, ncols=80, desc='Training')):
         # Set the gradients to zero before starting backpropagation
         optimizer.zero_grad()
@@ -40,26 +46,46 @@ def train_model():
         img, gt = img.to(device, dtype=torch.float), gt.to(device, dtype=torch.long)
 
         # Perform a feed-forward pass
-        logits = model(img)
+        #logits = model(img)
 
         # Compute the batch loss
-        loss = loss_fn(logits, gt)
+        #loss = loss_fn(logits, gt)
 
         # Compute gradient of the loss fn w.r.t the trainable weights
-        loss.backward()
+        #loss.backward()
 
         # Update the trainable weights
-        optimizer.step()
+        #optimizer.step()
+        with autocast('cuda'):
+            logits = model(img)
+            loss = loss_fn(logits, gt) / accumulation_steps
+        
+        scaler.scale(loss).backward()
+
+        if (i + 1) % accumulation_steps == 0:
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
 
         # Accumulate the batch loss
         train_loss += loss.item()
 
         # Accumulate the performance for every iteration
         seg_maps = logits.cpu().detach().numpy().argmax(axis=1)
+        prediction = logits.argmax(axis=1).cpu().numpy()
         gt = gt.cpu().detach().numpy()
         performance += getattr(metrics, cmd_args.metric)(seg_maps, gt)
+     train_metrics['iou'] += iou(prediction, gt)
+        train_metrics['accuracy'] += accuracy(prediction, gt)
+        train_metrics['precision'] += precision(prediction, gt)
+        train_metrics['recall'] += recall(prediction, gt)
+        train_metrics['f1'] += f1(prediction, gt)
 
-    return train_loss / len(train_loader), performance / len(train_loader)
+    for key in train_metrics:
+        train_metrics[key] /= len(train_loader)
+
+     
+    return train_loss / len(train_loader), performance / len(train_loader),train_metrics
 
 
 def validate_model():
@@ -70,6 +96,8 @@ def validate_model():
     model.eval()
     valid_loss = 0.0
     performance = 0
+    val_metrics = {'iou': 0, 'accuracy': 0, 'precision': 0, 'recall': 0, 'f1': 0}
+    
 
     with torch.no_grad():
         for i, (img, gt) in enumerate(valid_loader):
@@ -77,39 +105,49 @@ def validate_model():
             img, gt = img.to(device, dtype=torch.float), gt.to(device, dtype=torch.long)
 
             # Perform a feed-forward pass
-            logits = model(img)
+            #logits = model(img)
 
             # Compute the batch loss
-            loss = loss_fn(logits, gt)
+            #loss = loss_fn(logits, gt)
+            with autocast('cuda'):
+                logits = model(img)
+                loss = loss_fn(logits, gt)
 
             # Accumulate the batch loss
             valid_loss += loss.item()
 
             # Accumulate the performance for every iteration
             seg_maps = logits.cpu().detach().numpy().argmax(axis=1)
+            prediction = logits.argmax(axis=1).cpu().numpy()
             gt = gt.cpu().detach().numpy()
             performance += getattr(metrics, cmd_args.metric)(seg_maps, gt)
+     val_metrics['iou'] += iou(prediction, gt)
+            val_metrics['accuracy'] += accuracy(prediction, gt)
+            val_metrics['precision'] += precision(prediction, gt)
+            val_metrics['recall'] += recall(prediction, gt)
+            val_metrics['f1'] += f1(prediction, gt)
 
-    return valid_loss / len(valid_loader), performance / len(valid_loader)
+    for key in val_metrics:
+        val_metrics[key] /= len(valid_loader)
+
+    return valid_loss / len(valid_loader), performance / len(valid_loader),val_metrics
 
 
 if __name__ == "__main__":
 
     # 1. Parse the command arguments
-    args = argparse.ArgumentParser(description='Train a deep model for iris segmentation')
-    args.add_argument('-d', '--dataset', default='dataset', type=str, help='Dataset folder')
-    args.add_argument('-e', '--epochs', default=100, type=int, help='Number of epochs')
-    args.add_argument('-b', '--batch-size', default=8, type=int, help='Batch size')
-    args.add_argument('-i', '--img-size', default=480, type=int, help='Image size')
-    args.add_argument('-c', '--checkpoint', default='segmentattention/train/checkpoints', type=str,
-                      help='Checkpoint folder')
-    args.add_argument('-t', '--metric', default='iou', type=str, help='Metric for optimization')
+     parser = argparse.ArgumentParser(description='Train a deep model for shrimp segmentation')
+    parser.add_argument('-d', '--dataset', default="E:/thanh/ntu_group/phuong/segatten/train/dataset", type=str, help='Dataset folder')
+    parser.add_argument('-e', '--epochs', default=100, type=int, help='Number of epochs')
+    parser.add_argument('-b', '--batch-size', default=4, type=int, help='Batch size')
+    parser.add_argument('-i', '--img-size', default=480, type=int, help='Image size')
+    parser.add_argument('-c', '--checkpoint', default='checkpoints', type=str, help='Checkpoint folder')
+    parser.add_argument('-t', '--metric', default='iou', type=str, help='Metric for optimization')
 
-    cmd_args = args.parse_args()
+    cmd_args = parser.parse_args()
     device = setup_cuda()
 
-    # 2. Load the dataset
-    from utils.lane_dataset import LaneDataset
+    from utils.lanedatasetv2 import LaneDataset
 
     train_dataset = LaneDataset(dataset_dir=cmd_args.dataset, subset='test', img_size=cmd_args.img_size)
     train_loader = torch.utils.data.DataLoader(train_dataset,
@@ -117,7 +155,7 @@ if __name__ == "__main__":
                                                shuffle=True,
                                                num_workers=6)
 
-    valid_dataset = LaneDataset(dataset_dir=cmd_args.dataset, subset='test', img_size=cmd_args.img_size)
+    valid_dataset = LaneDataset(dataset_dir=cmd_args.dataset, subset='valid', img_size=cmd_args.img_size)
     valid_loader = torch.utils.data.DataLoader(valid_dataset,
                                                batch_size=cmd_args.batch_size,
                                                shuffle=True,
@@ -130,22 +168,51 @@ if __name__ == "__main__":
     # 4. Specify loss function and optimizer
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=0.001)
-
+    train_history = {'loss': [], 'iou': [], 'accuracy': [], 'precision': [], 'recall': [], 'f1': []}
+    val_history = {'loss': [], 'iou': [], 'accuracy': [], 'precision': [], 'recall': [], 'f1': []}
+    # 5. Start training the model
     # 5. Start training the model
     max_perf = 0
     for epoch in range(cmd_args.epochs):
         # 5.1. Train the model over a single epoch
-        _, train_perf = train_model()
+        train_loss, train_perf,train_metrics = train_model()
 
         # 5.2. Validate the model
-        _, valid_perf = validate_model()
+        val_loss, valid_perf,valid_metrics= validate_model()
 
         print('Epoch: {} \tTraining {}: {:.4f} \tValid {}: {:.4f}'.format(epoch, cmd_args.metric, train_perf,
                                                                           cmd_args.metric, valid_perf))
 
+        train_history['loss'].append(train_loss)
+        val_history['loss'].append(val_loss)
+        train_history['iou'].append(train_metrics['iou'])
+        val_history['iou'].append(val_metrics['iou'])
+        train_history['f1'].append(train_metrics['f1'])
+        val_history['f1'].append(val_metrics['f1'])
+        train_history['precision'].append(train_metrics['precision'])
+        val_history['precision'].append(val_metrics['precision'])
+        train_history['recall'].append(train_metrics['recall'])
+        val_history['recall'].append(val_metrics['recall'])
+        train_history['accuracy'].append(train_metrics['accuracy'])
+        val_history['accuracy'].append(val_metrics['accuracy'])
+
+        # 5.3. Save the model if the validation performance is increasing
+        path = "E:/thanh/ntu_group/phuong/segatten/train/checkpoints"
+        path2 = "E:/thanh/ntu_group/phuong/segatten/train/graph"
         # 5.3. Save the model if the validation performance is increasing
         if valid_perf > max_perf:
             print('Valid {} increased ({:.4f} --> {:.4f}). Model saved'.format(cmd_args.metric, max_perf, valid_perf))
-            torch.save(model.state_dict(), cmd_args.checkpoint + '/unet_resnet18_epoch_' + str(epoch) +
-                       '_' + cmd_args.metric + '_{0:.4f}'.format(valid_perf) + '.pt')
+            torch.save(model.state_dict(), f"{path}/unetresnetpre_epoch_{epoch}_{cmd_args.metric}_{valid_perf:.4f}.pt")
             max_perf = valid_perf
+    epochs_range = range(cmd_args.epochs)
+    for metric_name in train_history:
+        plt.figure()
+        plt.plot(epochs_range, train_history[metric_name], label=f'Training {metric_name}')
+        plt.plot(epochs_range, val_history[metric_name], label=f'Validation {metric_name}')
+        plt.xlabel('Epochs')
+        plt.ylabel(metric_name.capitalize())
+        plt.title(f'{metric_name.capitalize()} vs. Epochs')
+        plt.legend()
+        plt.grid(True)
+        plt.savefig(f"{path2}/unetresnetpre_{metric_name}.png")
+        plt.show()
